f4b6d663964963024462018ad6bafa8c069e8f18
Mukesh Otwani <mukeshotwani.50@gmail.com>
2025-12-10 12:21:25 +0530

    added more configs

--- src/main/java/factory/BrowserFactory.java
+++ src/main/java/factory/BrowserFactory.java
@@ -3,6 +3,8 @@ package factory;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.time.Duration;
+
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -13,142 +15,126 @@ import org.openqa.selenium.safari.SafariDriver;
 
 import dataproviders.ConfigReader;
 
-public class BrowserFactory 
-{
-	
-	static WebDriver driver;
-	
-	public static WebDriver getDriver()
-	{
-		return driver;
-	}
-	
+public class BrowserFactory {
 
-	public static WebDriver startBrowser(String browserName, String applicationURL) 
-	{
+    static WebDriver driver;
 
-		System.out.println("Application will be running on " + browserName + " with url " + applicationURL);
-		
-		String isCloud = ConfigReader.getValue("cloud");
-		String huburl = ConfigReader.getValue("huburl") + ":" + ConfigReader.getValue("hubport") + "/wd/hub";
+    public static WebDriver getDriver() {
+        return driver;
+    }
 
-		// NEW: Flag for GitHub Actions
-		String isGithubAction = ConfigReader.getValue("githuaction");
-		
-		
-		if(isCloud.equalsIgnoreCase("true"))
-		{
-			
-			System.out.println("**** Running Test On Selenium Grid ****");
-			
-			if (browserName.toLowerCase().equals("chrome") || browserName.toLowerCase().equals("gc")
-					|| browserName.toLowerCase().equals("google")) 
-			{
-				ChromeOptions opt = new ChromeOptions();
-				
-				// Use headless in GitHub Actions
-				if(isGithubAction != null && isGithubAction.equalsIgnoreCase("true"))
-				{
-					opt.addArguments("--headless");
-					opt.addArguments("--no-sandbox");
-					opt.addArguments("--disable-dev-shm-usage");
-				}
-				
-				try 
-				{
-					driver = new RemoteWebDriver(new URL(huburl), opt);
-					
-				} 
-				catch (MalformedURLException e) 
-				{
-					System.out.println("Could not connect to Grid");
-				}
-				
-			} 
-			else if (browserName.toLowerCase().equals("firefox") || browserName.toLowerCase().equals("mozila")
-					|| browserName.toLowerCase().equals("ff")) 
-			{
-				driver = new FirefoxDriver();
-			
-			} 
-			else if (browserName.toLowerCase().equals("edge") || browserName.toLowerCase().equals("msedge")
-					|| browserName.toLowerCase().equals("microsoft edge")) 
-			{
-				driver = new EdgeDriver();
-			}  
-			else 
-			{
-				System.out.println("Sorry - currently our framework does not support " + browserName + " browser");
-			}
-			
-			
-		}
-		else
-		{
-			
-			System.out.println("**** Running Test On Local Machine ****");
-			
-			if (browserName.toLowerCase().equals("chrome") || browserName.toLowerCase().equals("gc")
-					|| browserName.toLowerCase().equals("google")) 
-			{
-				// If running in GitHub Actions -> headless
-				if(isGithubAction != null && isGithubAction.equalsIgnoreCase("true"))
-				{
-					ChromeOptions opt = new ChromeOptions();
-					   	opt.addArguments("--headless=new");
-	                    opt.addArguments("--no-sandbox");
-	                    opt.addArguments("--disable-dev-shm-usage");
-	                    opt.addArguments("--window-size=1920,1080");
-	                    opt.addArguments("--high-dpi-support=1");
-	                    opt.addArguments("--force-device-scale-factor=1");
-					driver = new ChromeDriver(opt);
-				}
-				else
-				{
-					// Normal local run
-					driver = new ChromeDriver();
-				}
-			} 
-			else if (browserName.toLowerCase().equals("firefox") || browserName.toLowerCase().equals("mozila")
-					|| browserName.toLowerCase().equals("ff")) 
-			{
-				driver = new FirefoxDriver();
-			
-			} 
-			else if (browserName.toLowerCase().equals("edge") || browserName.toLowerCase().equals("msedge")
-					|| browserName.toLowerCase().equals("microsoft edge")) 
-			{
-				driver = new EdgeDriver();
-			} 
-			else if (browserName.toLowerCase().equals("safari") || browserName.toLowerCase().equals("apple safari")) 
-			{
-				driver = new SafariDriver();
-				
-			} 
-			else 
-			{
-				System.out.println("Sorry - currently our framework does not support " + browserName + " browser");
-			}
+    public static WebDriver startBrowser(String browserName, String applicationURL) {
 
-		}
-		
-		driver.manage().window().maximize();
-		
-		driver.manage().timeouts().pageLoadTimeout(
-				Duration.ofSeconds(Long.parseLong(ConfigReader.getValue("pageloadtimeout"))));
+        System.out.println("Application will be running on " + browserName + " with url " + applicationURL);
 
-		driver.get(applicationURL);
-		
-		driver.manage().timeouts().implicitlyWait(
-				Duration.ofSeconds(Long.parseLong(ConfigReader.getValue("implicitwait"))));
-		
-		driver.manage().timeouts().scriptTimeout(
-				Duration.ofSeconds(Long.parseLong(ConfigReader.getValue("scripttimeout"))));
-		
-		System.out.println("Application is up and running");
+        String isCloud = ConfigReader.getValue("cloud");
+        String huburl = ConfigReader.getValue("huburl") + ":" + ConfigReader.getValue("hubport") + "/wd/hub";
 
-		return driver;
-	}
+        // Flag for GitHub Actions
+        String githubFlag = ConfigReader.getValue("githuaction");
+        boolean isHeadless = githubFlag != null && githubFlag.equalsIgnoreCase("true");
 
+        if (isCloud.equalsIgnoreCase("true")) {
+
+            System.out.println("**** Running Test On Selenium Grid ****");
+
+            if (browserName.equalsIgnoreCase("chrome") || browserName.equalsIgnoreCase("gc")
+                    || browserName.equalsIgnoreCase("google")) {
+
+                ChromeOptions opt = new ChromeOptions();
+
+                if (isHeadless) {
+                    opt.addArguments("--headless=new");
+                    opt.addArguments("--no-sandbox");
+                    opt.addArguments("--disable-dev-shm-usage");
+                    opt.addArguments("--window-size=1920,1080");
+                    opt.addArguments("--high-dpi-support=1");
+                    opt.addArguments("--force-device-scale-factor=1");
+                }
+
+                try {
+                    driver = new RemoteWebDriver(new URL(huburl), opt);
+
+                } catch (MalformedURLException e) {
+                    System.out.println("Could not connect to Grid");
+                }
+
+            } else if (browserName.equalsIgnoreCase("firefox") || browserName.equalsIgnoreCase("mozila")
+                    || browserName.equalsIgnoreCase("ff")) {
+
+                driver = new FirefoxDriver();
+
+            } else if (browserName.equalsIgnoreCase("edge") || browserName.equalsIgnoreCase("msedge")
+                    || browserName.equalsIgnoreCase("microsoft edge")) {
+
+                driver = new EdgeDriver();
+
+            } else {
+                System.out.println("Sorry - currently our framework does not support " + browserName + " browser");
+            }
+
+        } else {
+
+            System.out.println("**** Running Test On Local Machine ****");
+
+            if (browserName.equalsIgnoreCase("chrome") || browserName.equalsIgnoreCase("gc")
+                    || browserName.equalsIgnoreCase("google")) {
+
+                if (isHeadless) {
+                    ChromeOptions opt = new ChromeOptions();
+                    opt.addArguments("--headless=new");
+                    opt.addArguments("--no-sandbox");
+                    opt.addArguments("--disable-dev-shm-usage");
+                    opt.addArguments("--window-size=1920,1080");
+                    opt.addArguments("--high-dpi-support=1");
+                    opt.addArguments("--force-device-scale-factor=1");
+
+                    driver = new ChromeDriver(opt);
+                } else {
+                    driver = new ChromeDriver();
+                }
+
+            } else if (browserName.equalsIgnoreCase("firefox") || browserName.equalsIgnoreCase("mozila")
+                    || browserName.equalsIgnoreCase("ff")) {
+
+                driver = new FirefoxDriver();
+
+            } else if (browserName.equalsIgnoreCase("edge") || browserName.equalsIgnoreCase("msedge")
+                    || browserName.equalsIgnoreCase("microsoft edge")) {
+
+                driver = new EdgeDriver();
+
+            } else if (browserName.equalsIgnoreCase("safari") || browserName.equalsIgnoreCase("apple safari")) {
+
+                driver = new SafariDriver();
+
+            } else {
+                System.out.println("Sorry - currently our framework does not support " + browserName + " browser");
+            }
+        }
+
+        // 👇 IMPORTANT PART
+        if (isHeadless) {
+            // maximize() headless me kaam nahi karta, isliye size hard-set karo
+            driver.manage().window().setSize(new Dimension(1920, 1080));
+        } else {
+            driver.manage().window().maximize();
+        }
+
+        driver.manage().timeouts().pageLoadTimeout(
+                Duration.ofSeconds(Long.parseLong(ConfigReader.getValue("pageloadtimeout"))));
+
+        driver.get(applicationURL);
+
+        driver.manage().timeouts().implicitlyWait(
+                Duration.ofSeconds(Long.parseLong(ConfigReader.getValue("implicitwait"))));
+
+        driver.manage().timeouts().scriptTimeout(
+                Duration.ofSeconds(Long.parseLong(ConfigReader.getValue("scripttimeout"))));
+
+        System.out.println("Application is up and running");
+
+        return driver;
+    }
 
 }
